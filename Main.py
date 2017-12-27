98426bddf0e6aae8c89c5810f58c87d7f1ffd3b1
Phan Truong Thanh <knightofk9@gmail.com>
2017-12-28 00:53:20 +0700

    Init

---
+++ Main.py
@@ -0,0 +1,8 @@
+import Constant
+from Detector import Detector
+
+
+# detector = Detector(Constant.DARK_NET_CFG_PATH + "yolo.cfg",  "yolo.weights",
+#                     Constant.DARK_NET_CFG_PATH + "coco.data")
+# detector.detect_all_image_in_folder(Constant.IMAGE_DETECT_INPUT_PATH,
+#                                     Constant.IMAGE_DETECT_OUTPUT_PATH)
